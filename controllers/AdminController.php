3db884148956f7f53b55f4e4d10e89f2f34fe742
chinh205 <chinhnvph52698@gmail.com>
2025-03-22 21:58:47 +0700

    Nội dung commit của bạn

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -41,4 +41,46 @@ class HomeController
         }
     }
 
+       // Danh Mục
+       public function formAddDm() {
+        require_once '../../views/Admins/DanhMuc/formAddDM.php';
+    }
+
+
+    public function listDm() {
+        $listDanhMuc = $this->modelAdmin->getAllDanhMuc();
+        require_once '../../views/Admins/DanhMuc/listDm.php';
+    }
+    public function postDm() {
+        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST)){
+            $name = $_POST['name'];
+            if($this->modelAdmin->postDm($name)){
+                header('location: router.php?act=listDm');
+                exit;
+            }
+        }
+    }
+    public function deleteDm() {
+            $id = $_GET['id'];
+            $record = $this->modelAdmin->getDmById($id);
+            if ($record) { // Kiểm tra xem bản ghi có tồn tại không
+                if ($this->modelAdmin->deleteDm($id)) {
+                    header('Location: router.php?act=listDm');
+                    exit;
+                } else {
+                    echo "Không thể xóa danh mục.";
+                }
+            } else {
+                echo "Danh mục không tồn tại.";
+            }
+    }
+
+
+    public function formSuaDm() {
+        $id = $_GET['id'];
+        $danhMuc = $this->modelAdmin->getDmById($id);
+        require_once '../../views/Admins/DanhMuc/updateDm.php';
+    }
+
+
 }
